86c371c84864aca6b34296a02fa2a8f46835aae5
Rutvik Patel <patelrutvik1702@gmail.com>
2021-08-10 23:46:26 -0400

    updates

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -24,7 +24,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/Rutvik17/Rutvik17">here</a>.</p>
+      <p> Source available <a href="https://github.com/Rutvik17/personal-site">here</a>.</p>
     </article>
   </Main>
 );
